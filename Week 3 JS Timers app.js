50592474f408b41bdbb30e278b5693eaaa8c8ff8
sarahroon <sarahrooney130500@gmail.com>
2026-01-19 12:06:23 +0000

    Update Week 3 Workshop Day 1 JS Timers app.js

--- Week 3 JS Timers app.js
+++ Week 3 JS Timers app.js
@@ -1,1 +1,10 @@
+const bombTimer = setTimeout(() => {
+  console.log("BOOM!")
+}, 5);
 
+// DEFUSE BOMB TIMER
+
+const defuse = document.getElementById("defuseBtn");
+defuse.addEventListener("click", () => {
+  clearTimeout(bombTimer);
+});
