acd4317d4ee0c75d8b53bb3120f35593fe2a6719
sarahroon <sarahrooney130500@gmail.com>
2026-01-19 11:55:04 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+// TIMER
+
+function say(Hello) {
+  console.log("Hello world!");
+}
+
+const myInterval = setInterval(() => {
+  console.log("I`m a message!");
+},  1000);
+
+setTimeout(() => {
+  clearInterval(myInterval);
+}, 5000);
+
+// BOMB TIMER
+
+const bombTimer = setTimeout(() => {
+  console.log("BOOM!")
+}, 1000);
+
+// DEFUSE BOMB TIMER
+
+const defuse = document.getElementById("defuseBtn");
+defuse.addEventListener("click", () => {
+  clearTimeout(bombTimer);
+});
+
+
 
