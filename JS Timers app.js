5bc5f598ea61d927bc6e4923bd27ee20008509ef
sarahroon <sarahrooney130500@gmail.com>
2026-01-19 12:58:27 +0000

    Delete JS Timers app.js

--- JS Timers app.js
+++
@@ -1,7 +0,0 @@
-function message() {
-  document.getElementById("displayMessage").style.display = "block";
-
-  setTimeout(function message() {
-    document.getElementById("displayMessage").style.display = "none";
-  }, 5000);
-}
